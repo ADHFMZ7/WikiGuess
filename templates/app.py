e50cdde1fa66a46a398ee32ed2a6bb62348aaaa8
dabren4 <darrenxychen@gmail.com>
2023-04-08 22:31:39 -0700

    updated index.html

--- templates/app.py
+++ templates/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
