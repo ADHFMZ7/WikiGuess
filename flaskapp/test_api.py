ef3165789c361cccc608814c4e349fcac4447bd1
NateGries1 <nategries@DESKTOP-8N0CO26>
2023-04-08 21:32:47 -0700

    Test Api

--- flaskapp/test_api.py
+++ flaskapp/test_api.py
@@ -3,6 +3,7 @@ from gpt import init_api, gpt_api_call
 
 init_api("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")
 
+#output = gpt_api_call("NATO", "Bosnia and Herzegovina intervention", 3000)
 
 print(gpt_api_call("NATO", "Bosnia and Herzegovina intervention", 3000))
 
