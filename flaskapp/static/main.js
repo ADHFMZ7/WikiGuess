303f51bad8ee864e5f0779b5ae9c5864a3a0569b
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-04-09 13:29:10 -0700

    add score

--- flaskapp/static/main.js
+++ flaskapp/static/main.js
@@ -1,8 +1,17 @@
+var score = 0;
+
+
 function SetText(json) {
     // Generate a random integer between 1 and 10
-    console.log(json["gpt"])
-    let right = document.getElementById("rightbox");
-    let left = document.getElementById("leftbox");
+    let right = document.getElementById("rightBox");
+    let left = document.getElementById("leftBox");
+    let middle = document.getElementById("middleBox");
+
+    // make middle h1 title
+    // make middle h2 header
+    // make middle h2 score
+    
+
     right.style.display = "block";
     left.style.display = "block";
     if (json['rand'] == 1) {
@@ -26,10 +35,15 @@ function ClickRight(json) {
         //get new json
         //SetText()
         //update score
+        score++;
+        json = start_game();
     } else {
         //load wrong animations
         //show restart button
         //update highscore
+        post_score(score);
+        score = 0;
+        json = start_game();
     }
 }
 
@@ -39,10 +53,15 @@ function ClickLeft(json) {
         //get new json
         //SetText()
         //update score
+        score++;
+        json = start_game()
     } else {
         //load wrong animations
         //show restart button
         //update highscore
+        post_score(score)
+        score = 0;
+        json = start_game()
     }
 }
 
@@ -61,19 +80,42 @@ document.getElementById('start-btn').addEventListener('click', function() {
       console.error(error);
     });
   });
-  
-  function start_game() {
-    return fetch('/game')
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        return data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+ 
+function start_game() {
+  return fetch('/game')
+    .then(response => {
+    console.log("response");
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+
+function post_score(num) {
+  return fetch('/game', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({score: num})
+  })
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    return data;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
   
