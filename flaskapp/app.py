b1047cdc481d0ebcac1cc8261e2168c18bfec8d3
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-04-08 18:12:37 -0700

    start flask app

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -2,3 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+
+    title = get_random_title()
+
+    # Calls to get header, block of text
+    block = get_block_
+
+    
+    length = len(block)
+
+
+
+API_STRING = "For an article about %s, make a section titled %s that is %s characters long."
+
+
+
