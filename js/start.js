6c1946a9d34b7f2d57352905cf06c495a37381c6
NateGries1 <nategries@DESKTOP-8N0CO26>
2023-04-09 02:31:23 -0700

    commit

--- js/start.js
+++ js/start.js
@@ -1,9 +1,6 @@
 
-function ClickStart() {
-    right = document.getElementById("rightBox");
-    right.style.display = "block";
-    left = document.getElementById("leftBox");
-    left.style.display = "block";
+function start_game() {
+    startbox = document.getElementById("")
     SetText(/* some json */);
     
 }
