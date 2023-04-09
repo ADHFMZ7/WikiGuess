dc7587f71fae844dc5f5896db734f0880ef58c0f
Randy Do <randyydoo@csu.fullerton.edu>
2023-04-08 19:43:05 -0700

    progress

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+
+#create route for home page
 @app.route('/')
+def index():
+    return render_template('index.html')
+#create route for guess
+@app.route('/guess', methods = ['POST'])
+def guess():
+    guess_img = request.form['guess']
 def index():
 
     title = get_random_title()
@@ -17,5 +25,9 @@ def index():
 
 API_STRING = "For an article about %s, make a section titled %s that is %s characters long."
 
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
 
 
