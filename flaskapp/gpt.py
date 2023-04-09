7db4640565954cae83a53c238f50cf283ed0113d
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-04-08 19:58:55 -0700

    added requirements

--- flaskapp/gpt.py
+++ flaskapp/gpt.py
@@ -10,15 +10,19 @@ def gpt_api_call(title, header, count):
 
     api_prompt = """For an article about "ARTICLE", make a section titled "HEADER" that is about COUNT characters long. The section is a part of a larger article about "ARTICLE".  Send only the section and omit the section's header. Write it in the style of an encyclopedia."""
 
-    api_prompt.replace("ARTICLE", title)
-    api_prompt.replace("HEADER", header)
-    api_prompt.replace("COUNT", count)
+    api_prompt = api_prompt.replace("ARTICLE", title)
+    api_prompt = api_prompt.replace("HEADER", header)
+    api_prompt = api_prompt.replace("COUNT", str(count))
 
-    response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=api_prompt,
+    print(api_prompt)
+
+    response = openai.ChatCompletion.create(
+    model= "gpt-3.5-turbo-0301",
+    messages=[
+        {"role": "user", "content": api_prompt}
+    ],
     max_tokens=count)
-    return response.choices[0].text
+    return response.choices[0].content
 
 
 
--- flaskapp/test_api.py
+++ flaskapp/test_api.py
@@ -1,9 +1,9 @@
 from gpt import init_api, gpt_api_call
 
 
-init_api("")
+init_api("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")
 
 
-print(gpt_api_call("NATO", "Libya intervention", 2789))
+print(gpt_api_call("NATO", "Bosnia and Herzegovina intervention", 3000))
 
 
